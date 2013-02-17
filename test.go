c0a257e37b2d05dc4c53415d9f54319c9bfab088
Dan Tillberg <dan@tillberg.us>
2013-02-17 17:32:42 +0000

    some Architecure Astronautics

--- test.go
+++ test.go
@@ -17,33 +17,64 @@ var watch_target *string = flag.String("watch", "_sync", "The directory to keep 
 type Blob struct {
   bytes  []byte
   hash   []byte
-}
 
-type FileBlob struct {
-  Blob
-  path   string
-  parent *TreeBlob
-  // mode flags
-  // timestamps?
-}
-
-type TreeBlob struct {
-  Blob
+  // non-empty for top-level files and trees:
   path string
-  children map[string]*PathedBlob
-  // other properties?
+
+  // non-empty for non-leaf files and trees:
+  segments []*Blob
+
+  // lazily populated for tree roots:
+  // XXX ideally, this would be a B-Tree with distributed caching
+  children map[string]*Blob
+
+  // non-nil for everything except root trees:
+  parent *Blob
+
+  is_tree bool
+  is_file bool
+
+  // XXX for top level file: mode flags, timestamps?
+  // XXX for top level tree: ?
 }
 
-type PathedBlob interface {
-  IsFile() bool
-  Hash()   []byte
+// type Tree interface {
+
+//   NavigateTo(path string) Tree
+//   Remove(path string)
+//   Update(blob Blob)
+// }
+
+func (b Blob) FirstParent(f func(*Blob) bool) *Blob {
+  for p := b.parent; p != nil; p = p.parent {
+    if f(p) {
+      return p
+    }
+  }
+  return nil
 }
 
-type FileBlobUpdate struct {
-  FileBlob
-  exists bool
-  size   int64
+func (b Blob) ShareRoot() *Blob {
+  return b.FirstParent(func(b *Blob) bool {
+    return b.IsShareRoot()
+  })
 }
+
+func (b Blob) Root() *Blob {
+  root := b.FirstParent(func(b *Blob) bool {
+    return b.IsFile() || b.IsTree()
+  })
+  if root == nil {
+    panic("Blob has no root")
+  }
+  return root
+}
+
+func (b Blob) IsShareRoot()   bool { return b.parent == nil }
+func (b Blob) IsTree()        bool { return b.is_tree }
+func (b Blob) IsTreeSegment() bool { return b.Root().IsTree() }
+func (b Blob) IsFile()        bool { return b.is_file }
+func (b Blob) IsFileSegment() bool { return b.Root().IsFile() }
 
 func (blob Blob) Hash() []byte {
   if blob.hash == nil {
@@ -52,8 +83,11 @@ func (blob Blob) Hash() []byte {
   return blob.hash
 }
 
-func (b FileBlob) IsFile() bool { return true }
-func (b TreeBlob) IsFile() bool { return false }
+type FileUpdate struct {
+  Blob
+  exists bool
+  size   int64
+}
 
 func calculateHash(bytes []byte) []byte {
   SHA256_SALT_BEFORE := []byte{'s', 'h', 'a', 'r', 'e', 'd', '('}
@@ -65,21 +99,21 @@ func calculateHash(bytes []byte) []byte {
   return h.Sum([]byte{})
 }
 
-func notificationReporter(input chan FileBlobUpdate) {
+func notificationReporter(input chan FileUpdate) {
   for op := range input {
     fmt.Printf("%s %d %#x\n", op.path, op.size, op.Hash());
   }
 }
 
-func processChange(output chan FileBlobUpdate, path_channel chan string) {
+func processChange(output chan FileUpdate, path_channel chan string) {
   for path := range path_channel {
     statbuf, err := os.Stat(path)
     if err != nil {
-      output <- FileBlobUpdate{FileBlob{Blob{nil, nil}, path, nil}, false, 0}
+      output <- FileUpdate{Blob{nil, nil, path, nil, nil, nil, false, true}, false, 0}
     } else {
       bytes, err := ioutil.ReadFile(path)
       if err != nil { panic(err) }
-      output <- FileBlobUpdate{FileBlob{Blob{bytes, nil}, path, nil}, true, statbuf.Size()}
+      output <- FileUpdate{Blob{bytes, nil, path, nil, nil, nil, false, true}, true, statbuf.Size()}
     }
   }
 }
@@ -115,7 +149,7 @@ func main() {
   flag.Parse()
   fmt.Println("Started.")
   go restartOnChange()
-  var report_channel = make(chan FileBlobUpdate, 100)
+  var report_channel = make(chan FileUpdate, 100)
   var update_channel = make(chan string, 100)
   var debounce_channel = make(chan string, 100)
   go notificationReporter(report_channel)
