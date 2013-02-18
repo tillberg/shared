1af54363b5b07066ee29cd1669c315f10f2ab3ca
Dan Tillberg <dan@tillberg.us>
2013-02-18 23:03:04 +0000

    Added option to run a second copy of the dev script against a separate sync and cache folder

--- dev.sh
+++ dev.sh
@@ -3,6 +3,6 @@ mkdir -p sharedpb
 protoc -I=proto/ --go_out=sharedpb/ proto/shared.proto
 while :
 do
-    go run shared.go
+    go run shared.go --watch _sync$1 --cache _cache$1
     sleep 0.5
 done
