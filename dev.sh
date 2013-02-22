d92aa68faca2b9978fc58e39c3e1cb0c956ba696
Dan Tillberg <dan@tillberg.us>
2013-02-22 05:26:04 +0000

    Reorganized to start writing basic automated tests

--- dev.sh
+++ dev.sh
@@ -1,8 +1,8 @@
 go get code.google.com/p/goprotobuf/{proto,protoc-gen-go}
-mkdir -p sharedpb
-protoc -I=proto/ --go_out=sharedpb/ proto/shared.proto
+mkdir -p src/sharedpb
+protoc -I=proto/ --go_out=src/sharedpb/ proto/shared.proto
 while :
 do
-    go run shared.go --watch _sync$1 --cache _cache$1 --port 925$1
+    go run src/shared.go --watch _sync$1 --cache _cache$1 --port 925$1
     sleep 0.5
 done
