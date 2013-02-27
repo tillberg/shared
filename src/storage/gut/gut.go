b8c542cc31e94fc3e6b8afcb7d5933973dabc8be
Dan Tillberg <dan@tillberg.us>
2013-02-27 05:15:56 +0000

    Wrote (untested) Put and Get operations for storage/gut

--- src/storage/gut/gut.go
+++ src/storage/gut/gut.go
@@ -2,45 +2,56 @@
 package gut
 
 import (
-  "crypto/sha256"
+  "crypto/sha1"
+  "encoding/hex"
+  "fmt"
+  "io/ioutil"
+  "os"
+  "path"
   "../../types"
 )
 
 type Storage struct {
-  Root string
+  RootPath string
 }
 
-func (*Storage) Get(hash types.Hash) ([]byte, error) {
-  // cachePath := GetCachePath(hash)
-  // _, err := os.Stat(cachePath)
-  return nil, nil
+func (s *Storage) getCachePath(hash types.Hash) string {
+  hashString := hex.EncodeToString(hash)
+  return path.Join(s.RootPath, "objects", hashString[:2], hashString[2:])
 }
-
-func (*Storage) Put(bytes []byte)    (types.Hash, error) {
-  // Save a copy in the cache if we don't already have one
-  // cachePath := GetCachePath(blob.Hash())
-  // _, err := os.Stat(cachePath)
-  // if err != nil {
-  //   os.MkdirAll(path.Dir(cachePath), 0755)
-  //   ioutil.WriteFile(cachePath, blob.bytes, 0644)
-  //   log.Printf("Cached %s", blob.ShortHashString())
-  // }
-  return nil, nil
-}
-
-var SHA256_SALT_BEFORE = []byte{'s', 'h', 'a', 'r', 'e', 'd', '('}
-var SHA256_SALT_AFTER = []byte{')'}
 
 func calculateHash(bytes []byte) types.Hash {
-  h := sha256.New()
-  h.Write(SHA256_SALT_BEFORE)
+  h := sha1.New()
+  h.Write([]byte(fmt.Sprintf("blob %d", len(bytes))))
+  h.Write([]byte{0})
   h.Write(bytes)
-  h.Write(SHA256_SALT_AFTER)
   return h.Sum([]byte{})
 }
 
-// func GetCachePath(root string, hash types.Hash) string {
-//   hashString := GetHexString(hash)
-//   return path.Join(root, hashString[:2], hashString[2:])
-// }
+func (s *Storage) Get(hash types.Hash) (data []byte, err error) {
+  cachePath := s.getCachePath(hash)
+  _, err = os.Stat(cachePath)
+  if err != nil {
+    data, err = ioutil.ReadFile(cachePath)
+  }
+  return data, err
+}
 
+func (s *Storage) Put(data []byte) (hash types.Hash, err error) {
+  hash = calculateHash(data)
+  cachePath := s.getCachePath(hash)
+  _, err = os.Stat(cachePath)
+  if err != nil {
+    os.MkdirAll(path.Dir(cachePath), 0755)
+    err = ioutil.WriteFile(cachePath, data, 0644)
+  }
+  return hash, err
+}
+
+func (*Storage) PutRef(name string, hash types.Hash) error {
+  return nil
+}
+
+func (*Storage) GetRef(name string) (types.Hash, error) {
+  return nil, nil
+}
--- src/storage/storage.go
+++ src/storage/storage.go
@@ -11,6 +11,8 @@ import (
 type Storage interface {
   Get(hash types.Hash) ([]byte, error)
   Put(bytes []byte)    (types.Hash, error)
+  PutRef(name string, hash types.Hash) error
+  GetRef(name string) (types.Hash, error)
 }
 
 var CacheRoot = ""
@@ -21,7 +23,7 @@ func Configured() Storage {
   storage, err := config.GetString("main", "storage")
   types.Check(err)
   if storage == "gut" {
-    return Storage(&gut.Storage{Root: CacheRoot})
+    return Storage(&gut.Storage{RootPath: CacheRoot})
   } else {
     log.Fatalf("Unrecognized storage configured: %s", storage)
   }
